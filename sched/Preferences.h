772751e9e773698946378c211fed08549f2385b4
kevin birch <kmb@pobox.com>
2012-09-03 02:10:36 -0400

    cleanup sandbox

---
+++ sched/Preferences.h
@@ -0,0 +1,13 @@
+//
+//  Preferences.h
+//  sched
+//
+//  Created by kevin birch on 2012-09-02.
+//  Copyright (c) 2012年 com.webguys. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Preferences : NSWindowController
+
+@end
